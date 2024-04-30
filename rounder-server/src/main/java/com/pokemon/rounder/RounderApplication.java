6bc347edce546a335f38d927a323b7b3ec1eb71b
Chris Wren <chris.wren@bt.com>
2024-04-30 06:16:08 +0100

    Initial setup

---
+++ rounder-server/src/main/java/com/pokemon/rounder/RounderApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.rounder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RounderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RounderApplication.class, args);
+	}
+
+}
---
+++ rounder-server/src/test/java/com/pokemon/rounder/RounderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.rounder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RounderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
